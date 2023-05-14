5616430538e8a7886a4e61a59d1af73449ef6ed4
MooMan <36575231+Namesnipes@users.noreply.github.com>
2023-05-13 21:23:07 -0700

    project template

---
+++ GameFiles/testing.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public partial class testing : Node2D
+{
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        float AMOUNT = 5;
+        if (Input.IsActionPressed("up"))
+        {
+            this.Position += new Vector2(0, -AMOUNT);
+        }
+        if (Input.IsActionPressed("right"))
+        {
+            this.Position += new Vector2(AMOUNT, 0);
+        }
+        if (Input.IsActionPressed("left"))
+        {
+            this.Position += new Vector2(-AMOUNT, 0);
+        }
+        if (Input.IsActionPressed("down"))
+        {
+            this.Position += new Vector2(0, AMOUNT);
+        }
+    }
+}
