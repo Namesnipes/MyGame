7461749e36390f77384ffe326f60bd960455338e
MooMan <36575231+Namesnipes@users.noreply.github.com>
2023-05-15 23:40:00 -0700

    Fixed bug where clicking sprite was not registered

--- GameFiles/cat.cs
+++ GameFiles/cat.cs
@@ -3,32 +3,34 @@ using System;
 
 public partial class cat : Sprite2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-
-	}
-	public override void _Input(InputEvent @event)
-	{
-    if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed)
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
     {
-		window w = GetParent().GetNode<window>("window");
-		if(mouseEvent.Position.X <= this.Position.X + (float)this.Texture.GetWidth()/2 &&
-			mouseEvent.Position.X >= this.Position.X - (float)this.Texture.GetWidth()/2 &&
-			mouseEvent.Position.Y <= this.Position.Y + (float)this.Texture.GetHeight()/2 &&
-			mouseEvent.Position.Y >= this.Position.Y - (float)this.Texture.GetHeight()/2){
-
-		GD.Print("Clicked Cat");
-		w.SetClicked(true);
-		}
-		else{
-			w.SetClicked(false);
-		}
     }
-	}
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+
+    }
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed)
+        {
+            window w = GetParent().GetNode<window>("window");
+            GD.Print(this.GetGlobalMousePosition());
+            GD.Print(this.ToLocal(this.GetGlobalMousePosition()));
+            if (this.GetRect().HasPoint(this.ToLocal(this.GetGlobalMousePosition())))
+            {
+
+                GD.Print("Clicked Cat");
+                w.SetClicked(true);
+            }
+            else
+            {
+                w.SetClicked(false);
+            }
+        }
+    }
 }
